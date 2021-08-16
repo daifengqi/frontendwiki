7e983452406966637b7e1fce416115325d4dfbd4
youranreus <youranreus@qq.com>
2021-08-16 14:59:01 +0800

    重新设计nav组件

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,12 +5,19 @@ function Nav() {
   return (
     <>
       <div className={styles.nav}>
-        <a className={styles.navElement} href="./index.html">
-          主页 Homepage
-        </a>
-        <a className={styles.navElement} href="./user.html">
-          个人 Info
-        </a>
+        <div className={styles.navContent}>
+          <a href="./index.html" className={styles.navElement}>
+            Frontend Wiki
+          </a>
+          <div>
+            <a className={styles.navElement} href="./index.html">
+              主页
+            </a>
+            <a className={styles.navElement} href="./user.html">
+              个人
+            </a>
+          </div>
+        </div>
       </div>
     </>
   );
