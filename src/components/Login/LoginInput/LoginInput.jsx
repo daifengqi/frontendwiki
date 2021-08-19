a8a62ca0bad86f9bc8c821125bd6e31bec1c6f64
zhangyan <251773659@qq.com>
2021-08-20 00:06:26 +0800

    input-post-start

--- src/components/Login/LoginInput/LoginInput.jsx
+++ src/components/Login/LoginInput/LoginInput.jsx
@@ -1,15 +1,54 @@
 import React from "react";
+import $ from 'jquery'
 import styles from "./LoginInput.module.css";
 
 class LoginInput extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+          username: '',
+          email: '',
+          password: ''
+        }
+        this.handleChangeName = this.handleChangeName.bind(this);
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    读取输入的内容
+    handleChangeName(event) {
+      this.setState({username: event.target.value});
+    }
+    handleChangeEmail(event) {
+      this.setState({email: event.target.value});
+    }
+    handleChangePassword(event) {
+      this.setState({password: event.target.value});
+    }
+    
+    点击submit触发的事件
+    handleSubmit(event) {
+      // if(this.state.userName.length >= 20) {
+      //   return alert("用户名不能超过20个字符，请重新输入")
+      // }
+      event.preventDefault();
+      event.preventDefault();
+      let username =this.state.username
+      let password =this.state.password
+      let email =this.state.email
+      let hash = {username, password, email}
+      $.post('user/register', hash) // 第一个参数与是路径，第二个参数要传给服务端的数据
+        .then(() => {
+          alert('success')
+        }, () => {
+          alert('error')
+        })
     }
 
     render() {
 
+
       let isLogin = this.props.login;
-      
       if(isLogin === 'login'){
         return (
           <>
@@ -32,7 +71,12 @@ class LoginInput extends React.Component {
                     </div>
                     <div className={styles.buttons}>
                       <button className={styles.buttomForgot} type="button">忘记密码?</button>
-                      <input className={styles.buttonsAction} type="submit" value="登录"></input>
+                      <input
+                        className={styles.buttonsAction}
+                        type="submit" value="登录"
+                        onClick={this.handleSignup}
+                      >
+                      </input>
                     </div>
                   </form>
                 </div>
@@ -46,22 +90,28 @@ class LoginInput extends React.Component {
                 <div className={styles.optionsFormLeft} id='bounceRight'>
                   <div className={styles.formLogin} id='signup'>
                     <h1 className={styles.title}>账号注册</h1>
-                    <form>
+                    <form onSubmit={this.handleSubmit} id='register-form'>
                       <div>
                         <div>
                             <input type="text"
-                              placeholder="昵称"
-                            className={styles.formInput}></input>
+                              placeholder="用户名"
+                              className={styles.formInput}
+                             username={this.state.username} onChange={this.handleChangeName}
+                            ></input>
                         </div>
                         <div>
                           <input type="email"
                             placeholder="邮箱"
-                          className={styles.formInput}></input>
+                            className={styles.formInput}
+                            email={this.state.email} onChange={this.handleChangeEmail}
+                          ></input>
                         </div>
                         <div>
                           <input type="password"
                             placeholder="输入密码"
-                          className={styles.formInput}></input>
+                            className={styles.formInput}
+                            password={this.state.password} onChange={this.handleChangePassword}
+                          ></input>
                         </div>
                       </div>
                       <div className={styles.buttons}>
