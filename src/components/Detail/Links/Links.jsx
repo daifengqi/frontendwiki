df3f27945f8c0bb0f961b9025046592b866d962b
2huo <maipianlaoxu@gmail.com>
2021-08-19 21:13:48 +0800

    update

--- src/components/Detail/Links/Links.jsx
+++ src/components/Detail/Links/Links.jsx
@@ -2,31 +2,33 @@ import React, { useState } from "react";
 import styles from "./Links.module.css";
 
 function Links(props) {
+  const [cntLink, setCntLink] = useState("");
 
-    const [cntLink, setCntLink] = useState("")
+  function changeLink(url) {
+    setCntLink(url);
+    console.log(url);
+  }
 
-    function changeLink(url) {
-        setCntLink(url)
-        console.log(url)
-    }
-
-    return (
-        <>
-            <ul className={styles.linksList}>
-                {props.linkList.map((link) => {
-                    return (
-                        <li key={link.url} onClick={() => changeLink(link.url)}>
-                            <div className={styles.link}>
-                                <a target="_blank" className={styles.linkText} href={link.url}>{link.url}</a>
-                                <div className={styles.linkDesc}>{link.linkDesc}</div>
-                            </div>
-                        </li>
-                    )
-                })}
-            </ul>
-
-        </>
-    )
+  return (
+    <>
+      <ul className={styles.linksList}>
+        {props.linkList.map((link) => {
+          return (
+            <li
+              className={styles.link}
+              key={link.url}
+              onClick={() => changeLink(link.url)}
+            >
+              <a target="_blank" className={styles.linkText} href={link.url}>
+                <p>{link.url}</p>
+              </a>
+              <div className={styles.linkDesc}>{link.desc}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default Links;
