dcb96c1a7e5d5f97825eec9cb3ba1943e9faf9a3
youranreus <youranreus@qq.com>
2021-08-19 13:53:54 +0800

    将Sortbar, List, LinkItem组件组合，初步完成展示功能

--- src/components/Info/Components/Sortbar/Sortbar.jsx
+++ src/components/Info/Components/Sortbar/Sortbar.jsx
@@ -1,20 +1,55 @@
 import React from 'react';
 import styles from './Sortbar.module.css';
+import List from "../List/List.jsx";
 
 class Sortbar extends React.Component {
+
     constructor(props) {
         super(props);
+        let {data} = this.props;
+        let [...OriginData] = data;
+        this.state = {
+            Display: OriginData,
+            Thumb: data.sort(this.cmp),
+            Default: OriginData,
+        };
+    }
+
+    cmp = (a, b) => {
+        if (a.thumbs < b.thumbs)
+            return 1;
+        else if (a.thumbs > b.thumbs)
+            return -1;
+        else
+            return 0;
+    }
+
+    switch2Thumb = ()=> {
+        this.setState({
+            Display: this.state.Thumb
+        });
+    }
+
+    switch2Default = ()=> {
+        this.setState({
+            Display: this.state.Default
+        })
     }
 
     render() {
         return (
             <div className={styles.Sortbar}>
-                <div className={styles.left}>
-                    <span>最多点赞</span>
-                    <span>最新发布</span>
+                <div className={styles.header}>
+                    <div className={styles.left}>
+                        <span onClick={this.switch2Thumb.bind(this)}>最多点赞</span>
+                        <span onClick={this.switch2Default.bind(this)}>最新发布</span>
+                    </div>
+                    <div className={styles.right}>
+                        共 {this.state.data.length} 项
+                    </div>
                 </div>
-                <div className={styles.right}>
-                    共 777 项
+                <div className={styles.content}>
+                    <List data={this.state.Display}/>
                 </div>
             </div>
         );
