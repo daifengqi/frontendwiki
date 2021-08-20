5cef718f2096d346c76e07fc25f515d8b9f6297c
youranreus <youranreus@qq.com>
2021-08-20 21:25:46 +0800

    优化Nav组件为使用useLocation判断背景颜色

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,52 +2,51 @@ import React from "react";
 import classNames from 'classnames';
 import styles from "./nav.module.css";
 
-import { HashRouter as Router, Link } from 'react-router-dom';
+import {HashRouter as Router, Link, useLocation} from 'react-router-dom';
 
-class Nav extends React.Component {
-    constructor(props) {
-        super(props);
-        this.props = props;
+
+function Nav() {
+    let navStyle = [styles.nav];
+    let loginDisplay = 'none';
+
+    try {
+        let Location = useLocation();
+        navStyle.push(styles.dark)
+        loginDisplay = 'inline-block';
+    }
+    catch (error){
+        console.log('非个人页面')
     }
 
-    render() {
-        let navStyle = [styles.nav];
-        let loginDisplay = 'none';
-        const { page } = this.props;
-        if(page === 'Info')
-        {
-            navStyle.push(styles.dark)
-            loginDisplay = 'inline-block';
-        }
 
-        return (
-            <>
-                <div className={classNames(navStyle)}>
-                    <div className={styles.navContent}>
-                        <a href="./index.html" className={classNames(styles.navElement, styles.left)}>
-                            Frontend Wiki
+    return (
+        <>
+            <div className={classNames(navStyle)}>
+                <div className={styles.navContent}>
+                    <a href="./index.html" className={classNames(styles.navElement, styles.left)}>
+                        Frontend Wiki
+                    </a>
+                    <div>
+                        <a className={classNames(styles.navElement, styles.right)} href="./index.html">
+                            主页
                         </a>
-                        <div>
-                            <a className={classNames(styles.navElement, styles.right)} href="./index.html">
-                                主页
-                            </a>
-                            <a className={classNames(styles.navElement, styles.right)} href="./user.html">
-                                个人
-                            </a>
+                        <a className={classNames(styles.navElement, styles.right)} href="./user.html">
+                            个人
+                        </a>
 
-                            <Router>
-                                <Link
-                                    to="/login"
-                                    className={classNames(styles.navElement, styles.right)}
-                                    style={{display: loginDisplay}}
-                                >登录/注册</Link>
-                            </Router>
-                        </div>
+                        <Router>
+                            <Link
+                                to="/login"
+                                className={classNames(styles.navElement, styles.right)}
+                                style={{display: loginDisplay}}
+                            >登录/注册</Link>
+                        </Router>
                     </div>
                 </div>
-            </>
-        );
-    }
+            </div>
+        </>
+    );
 }
+
 
 export default Nav;
