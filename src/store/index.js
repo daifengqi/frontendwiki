c345b33961c32ffe46cbc780f6544d12d696e3f1
2huo <maipianlaoxu@gmail.com>
2021-08-20 16:34:07 +0800

    添加对redux插件的支持，方便调试

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,12 @@ import thunk from "redux-thunk";
 import rootReducer from "../reducers";
 import axios from "axios";
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
+// 添加支持浏览器redux插件
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk)),
+);
 
 export default store;
