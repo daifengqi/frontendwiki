23816304ecebdb9aef7a3a2400dfde87b12ec3f6
Cedric Qi <daifengqi@stu.pku.edu.cn>
2021-08-20 23:21:38 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -18,23 +18,23 @@ API.interceptors.request.use((req) => {
  */
 export const login = (formData) => API.post("/user/login", formData);
 export const register = (formData) => API.post("/user/register", formData);
-export const getUserInfo = () => API.get("/user/:id");
+export const getUserInfo = (id) => API.get(`/user/${id}`);
 
 /* 用户展示：点赞列表、收藏列表、浏览历史 */
-export const getThumbedLinks = () => API.get("/user/:id/thumbed");
-export const getCollectedLinks = () => API.get("/user/:id/collected");
-export const getVisitedLinks = () => API.get("/user/:id/visited");
+export const getThumbedLinks = (id) => API.get(`/user/${id}/thumbed`);
+export const getCollectedLinks = (id) => API.get(`/user/${id}/collected`);
+export const getVisitedLinks = (id) => API.get(`/user/${id}/visited`);
 
 /* 用户行为：点赞、收藏、浏览 */
 /**
  * @param {*} formData {linkId}
  */
-export const thumbLink = (formData) =>
-  API.post("/user/:id/thumbLink", formData);
-export const collectLink = (formData) =>
-  API.post("/user/:id/collectLink", formData);
-export const visitLink = (formData) =>
-  API.post("/user/:id/visitLink", formData);
+export const thumbLink = (formData, id) =>
+  API.post(`/user/${id}/thumbLink`, formData);
+export const collectLink = (formData, id) =>
+  API.post(`/user/${id}/collectLink`, formData);
+export const visitLink = (formData, id) =>
+  API.post(`/user/${id}/visitLink`, formData);
 
 /**
  *  links
