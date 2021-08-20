1aeab67f8301899517723f0a8a82f9ee1bd44e3f
GrinZero <774933704@qq.com>
2021-08-20 15:35:58 +0800

    link redux finished?maybe

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import {combineReducers} from "redux";
 
 import linkReducer from "./linkReducer";
+import commentReducer from "./commentReducer";
 import userReducer from "./userReducer";
 
 const rootReducer = combineReducers({
     linkReducer,
     userReducer,
+    commentReducer
 });
 
 export default rootReducer;
