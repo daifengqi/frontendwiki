b6d13502a71899e5d6890bdc125310319299e2dd
Cedric Qi <daifengqi@stu.pku.edu.cn>
2021-08-22 23:14:19 +0800

    Update treeNode.js

--- src/treeNode/treeNode.js
+++ src/treeNode/treeNode.js
@@ -1,53 +1,224 @@
-export const treeData=[
-    {
-      content: "css",
-      id: "1",
-      level: 0,
-      childrens: [
-        {
-          content: "动画属性",
-          id: "1-1",
-          level: 1,
-          childrens: [
-            {
-              content: "transition",
-              id: "1-1-1",
-              level: 2,
-              childrens: [],
-            },
-            {
-              content: "animation",
-              id: "1-1-2",
-              level: 2,
-              childrens: [],
-            },
-            {
-              content: "贝塞尔函数",
-              id: "1-1-3",
-              level: 2,
-              childrens: [],
-            },
-          ],
-        },
-        { content: "选择器", id: "1-2", level: 1, childrens: [] },
-      ],
-    },
-    {
-      content: "HTML",
-      id: "2",
-      level: 0,
-      childrens: [
-        {
-          content: "标签集",
-          id: "2-1",
-          level: 1,
-          childrens: [
-            { content: "div", id: "2-1-1", level: 2, childrens: [] },
-          ],
-        },
-        { content: "规范", id: "2-2", level: 1, childrens: [] },
-      ],
-    },
-    { content: "React", id: "3", level: 0, childrens: [] },
-    { content: "JS", id: "4", level: 0, childrens: [] },
-  ]
+export const treeData = [
+  {
+    content: "HTML",
+    id: "1",
+    level: 0,
+    childrens: [],
+  },
+  {
+    content: "CSS",
+    id: "2",
+    level: 0,
+    childrens: [
+      {
+        content: "布局",
+        id: "2-1",
+        level: 1,
+        childrens: [],
+      },
+      {
+        content: "动态效果",
+        id: "2-2",
+        level: 1,
+        childrens: [],
+      },
+    ],
+  },
+  {
+    content: "JavaScript",
+    id: "3",
+    level: 0,
+    childrens: [
+      { content: "ES6", id: "3-1", level: 1, childrens: [] },
+      { content: "期约与异步", id: "3-2", level: 1, childrens: [] },
+    ],
+  },
+  {
+    content: "前端框架",
+    id: "4",
+    level: 0,
+    childrens: [
+      {
+        content: "React",
+        id: "4-1",
+        level: 1,
+        childrens: [
+          {
+            content: "状态管理",
+            id: "4-1-1",
+            level: 2,
+            childrens: [
+              { content: "Redux", id: "4-1-1-1", level: 3, childrens: [] },
+              { content: "Mobx", id: "4-1-1-2", level: 3, childrens: [] },
+            ],
+          },
+          {
+            content: "源码解析",
+            id: "4-1-2",
+            level: 2,
+            childrens: [],
+          },
+          {
+            content: "NextJS",
+            id: "4-1-3",
+            level: 2,
+            childrens: [],
+          },
+        ],
+      },
+      { content: "VUE", id: "4-2", level: 1, childrens: [] },
+      { content: "Angular", id: "4-3", level: 1, childrens: [] },
+    ],
+  },
+  {
+    content: "图形与可视化",
+    id: "5",
+    level: 0,
+    childrens: [
+      {
+        content: "canvas",
+        id: "5-1",
+        level: 1,
+        childrens: [],
+      },
+      {
+        content: "svg",
+        id: "5-2",
+        level: 1,
+        childrens: [],
+      },
+      {
+        content: "WebGL",
+        id: "5-3",
+        level: 1,
+        childrens: [],
+      },
+    ],
+  },
+  {
+    content: "NodeJS",
+    id: "6",
+    level: 0,
+    childrens: [
+      {
+        content: "框架",
+        id: "6-1",
+        level: 1,
+        childrens: [
+          {
+            content: "express",
+            id: "6-1-1",
+            level: 2,
+            childrens: [],
+          },
+          {
+            content: "koa",
+            id: "6-1-2",
+            level: 2,
+            childrens: [],
+          },
+        ],
+      },
+      {
+        content: "源码",
+        id: "6-2",
+        level: 1,
+        childrens: [],
+      },
+    ],
+  },
+  {
+    content: "浏览器",
+    id: "7",
+    level: 0,
+    childrens: [
+      {
+        content: "渲染原理",
+        id: "7-1",
+        level: 1,
+        childrens: [],
+      },
+      {
+        content: "兼容性",
+        id: "7-2",
+        level: 1,
+        childrens: [],
+      },
+      {
+        content: "Web安全",
+        id: "7-3",
+        level: 1,
+        childrens: [],
+      },
+    ],
+  },
+  {
+    content: "跨端开发",
+    id: "8",
+    level: 0,
+    childrens: [
+      {
+        content: "移动端",
+        id: "8-1",
+        level: 1,
+        childrens: [],
+      },
+      {
+        content: "Hybrid",
+        id: "8-2",
+        level: 1,
+        childrens: [],
+      },
+      {
+        content: "小程序",
+        id: "8-3",
+        level: 1,
+        childrens: [],
+      },
+      {
+        content: "flutter",
+        id: "8-4",
+        level: 1,
+        childrens: [],
+      },
+    ],
+  },
+  {
+    content: "大前端工程化",
+    id: "9",
+    level: 0,
+    childrens: [
+      {
+        content: "构建工具",
+        id: "9-1",
+        level: 1,
+        childrens: [
+          {
+            content: "Webpack",
+            id: "9-1-1",
+            level: 2,
+            childrens: [],
+          },
+          {
+            content: "Vite",
+            id: "9-1-2",
+            level: 2,
+            childrens: [],
+          },
+        ],
+      },
+      {
+        content: "CI/CD",
+        id: "9-2",
+        level: 1,
+        childrens: [],
+      },
+      {
+        content: "性能监控",
+        id: "9-3",
+        level: 1,
+        childrens: [],
+      },
+    ],
+  },
+];
