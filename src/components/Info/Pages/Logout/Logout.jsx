035cf3af7436c29076d300d707bf2024ddb83c6c
youranreus <youranreus@qq.com>
2021-08-22 18:43:50 +0800

    添加登出

---
+++ src/components/Info/Pages/Logout/Logout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import common from '../../info.module.css'
+
+function Logout(){
+    return (
+        <h3 className={common.notice}>你已登出</h3>
+    );
+}
+
+export default Logout;
