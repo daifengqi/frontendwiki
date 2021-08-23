bc5a5aa54e8a4df91e23e7fa5aa3c739000b4642
youranreus <youranreus@qq.com>
2021-08-23 10:22:27 +0800

    页面添加淡入效果

--- src/components/Info/Pages/Logout/Logout.jsx
+++ src/components/Info/Pages/Logout/Logout.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import common from '../../info.module.css'
+import classNames from "classnames";
 
 function Logout(){
     return (
-        <h3 className={common.notice}>你已登出</h3>
+        <h3 className={classNames(common.notice,"animate__animated", "animate__fadeIn")}>你已登出</h3>
     );
 }
 
